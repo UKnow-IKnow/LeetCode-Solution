34e6f2eb79bb10a172297ae6f8341e75ac5c9c40
UKnow-IKnow <sarojsahu014@gmail.com>
2022-04-08 22:35:00 +0530

    Solution of Add Digits LeetCode(Q.258)

---
+++ Add_Digits.cpp
@@ -0,0 +1,19 @@
+//LeetCode Q.258. Add Digits
+
+//only logic part
+
+class Solution {
+public:
+    int addDigits(int num) {
+        int sum=0;
+        while(num)
+        {
+            sum+=(num%10);
+            num/=10;
+        }
+        if(sum<10)
+            return sum;
+        else
+            return addDigits(sum);
+    }
+};
