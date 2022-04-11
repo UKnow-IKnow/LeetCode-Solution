4920a37202af0fec4f6ee35cda454f7a12a792b9
UKnow-IKnow <sarojsahu014@gmail.com>
2022-04-11 20:58:31 +0530

    Solution of Arranging Coins LeetCode(Q.441)

---
+++ Arranging_Coins.cpp
@@ -0,0 +1,22 @@
+// LeetCode Q.441. Arranging Coins
+
+//Only logic part
+
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        if(n <= 2)
+            return 1;
+        
+        int level = 0;
+        for(int i=1; i<=n; i++)
+        {
+            n -= i;
+            level++;
+            if(n<=0)
+                return level;
+        }
+        
+        return level;
+    }
+};
