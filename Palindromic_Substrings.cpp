4bef0239a6f345fe824615c83ac28fb3a4eb3c93
UKnow-IKnow <sarojsahu014@gmail.com>
2022-05-22 17:08:24 +0530

    Solution of Palindromic Substrings LeetCode(Q.647)

---
+++ Palindromic_Substrings.cpp
@@ -0,0 +1,26 @@
+//LeetCode Q.647. Palindromic Substrings
+
+// only logic part
+
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int ans = 0;
+		for(int i = 0; i < s.size(); i++) {
+			int left = i, right = i;
+			while(left >= 0 && right < s.size() && s[left] == s[right]) {
+				ans++;
+				left--;
+				right++;
+			}
+
+			left = i, right = i + 1;
+			while(left >= 0 && right < s.size() && s[left] == s[right]) {
+				ans++;
+				left--;
+				right++;
+			}
+		}
+		return ans;
+    }
+};
