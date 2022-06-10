3261d2f21100456fbd1e5fb3df70e36c729b176b
UKnow-IKnow <sarojsahu014@gmail.com>
2022-06-10 23:12:03 +0530

    Solution of Longest Substring Without Repeating Characters LeetCode(Q.3)

---
+++ Longest_Substring_Without_Repeating_Characters.cpp
@@ -0,0 +1,21 @@
+//LeetCode Q.3. Longest Substring Without Repeating Characters
+
+//Only Logic Part
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int freq[256]={0};
+        int l=0,r=0,ans=0,n=s.size();
+        while(r<n){
+            freq[s[r]]++;
+            while(freq[s[r]]>1){
+                freq[s[l]]--;
+                l++;
+            }
+            ans = max(ans,r-l+1);
+            r++;
+        }
+        return ans;
+    }
+};
